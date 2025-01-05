bf081a929d7549591ab7ab744617345f9e6f246e
sandemoit <infosandemo@gmail.com>
2025-01-05 21:53:37 +0700

    refactor web

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DigiflazzController;
 use App\Http\Middleware\XSS;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,22 +16,7 @@ Route::middleware(XSS::class)->group(function () {
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
 
-    // Route::post('/webhook', [DigiflazzController::class, 'handle']);
-    Route::post('/webhook', function (Request $request) {
-        $secret = 'sanspaysecret'; // Ganti dengan secret yang sesuai
-
-        $post_data = file_get_contents('php://input');
-        $signature = hash_hmac('sha1', $post_data, $secret);
-
-        // Cek signature
-        if ($request->header('X-Hub-Signature') == 'sha1=' . $signature) {
-            // Proses data transaksi
-            Log::info(json_decode($request->getContent(), true));
-            return response()->json(['status' => 'success']);
-        } else {
-            return response()->json(['status' => 'unauthorized'], 401);
-        }
-    });
+    Route::post('/webhook', [DigiflazzController::class, 'handle']);
 
     require __DIR__ . '/auth.php';
     require __DIR__ . '/admin.php';
