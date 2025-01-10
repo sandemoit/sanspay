8ce6ba5a0c8ad6b99a174a74b2d645ce2d9d251a
sandemoit <infosandemo@gmail.com>
2025-01-10 09:44:13 +0700

    refix tampilan mobile

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,6 +8,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark-theme">
 
 <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>{{ configWeb('title')->value . ' | ' . $title ?? '' }}</title>
     <meta name="description" content="{{ configWeb('web_description')->value }}">
     <meta name="keywords"
